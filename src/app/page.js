41323392647ccffca1e924a404c2c24f3b0a0910
Hacid30 <hectorhacid@gmail.com>
2026-08-19 12:40:03 -0500

    feat: add MovieCard component and responsive grid for trending movies

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import MovieCard from "@/components/MovieCard";
 
 async function getTrendingMovies() {
-  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}&language=es-CO`);
+  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}&language=es-CO`,
+    { next: { revalidate: 3600 } }
+  );
 
   if (!res){
     throw new Error('Error al conectar con TMDB');
@@ -18,11 +20,11 @@ export default async function Home() {
       <main className="bg-slate-900 min-h-screen p-10">
         <h1 className="text-4xl font-bold text-sky-400 mb-6">Tendencias Hoy 🍿</h1>
         
-        <ul className="text-slate-300 space-y-2">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
           {movies.map((movie) => (
-            <li key={movie.id}>🎥 {movie.title}</li>
+            <MovieCard key={movie.id} movie={movie}  />
           ))}
-        </ul>
+        </div>
       </main>
   );
 }
---
+++ src/components/MovieCard.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function MovieCard({ movie }) {
+    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+    return(
+        <div className="bg-slate-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 flex flex-col justify-between">
+            <div className="relative w-full h-80">
+                <Image
+                    src={posterUrl}
+                    alt={movie.title}
+                    fill
+                    className="object-cover"
+                    sizes="(max-width: 768px) 100vw, (min-width: 1200px) 50vw, 25vw"
+                />
+            </div>
+
+            <div className="p-4 flex flex-col gap-2">
+                <h2 className="font-bold text-white text-lg line-clamp-1" >{movie.title}</h2>
+                <div className="flex justify-between items-center text-sm text-slate-400">
+                    <span>⭐ {movie.vote_average ? movie.vote_average.toFixed(1) : 'N/A'}</span>
+                    <span>{movie.release_date ? movie.release_date.split('-')[0] : 'S/F'}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
